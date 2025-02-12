dd84468bf7aba04cba4ead4de18cef0425a13fe8
jentina7 <jenishbekovatina@gmail.com>
2025-02-12 11:51:45 +0600

    updated

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def find_max(lst):
-    max_num =lst[0]
-    for i in numbers:
-        if i > max_num:
-            max_num = i
-    return max_num
-
-numbers = [1, 1, 23, 4, 42, 52, 32, 26]
-print(find_max(numbers))
+# def find_max(lst):
+#     max_num =lst[0]
+#     for i in numbers:
+#         if i > max_num:
+#             max_num = i
+#     return max_num
+#
+# numbers = [1, 1, 23, 4, 42, 52, 32, 26]
+# print(find_max(numbers))
